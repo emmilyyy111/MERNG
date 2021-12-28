5e6b09bcba67a551318857ebb995bd4c5233e7f3
emilymorgenbesser <emorgenbesser@sigmasolve.net>
2021-12-28 13:38:22 -0500

    server running locally

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
-
 const typeDefs = gql`
-    type Query
+    type Query{
+        sayHi: String!
+    }
 `
-emily
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello Emily!'
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen({ port: 5000 })
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
